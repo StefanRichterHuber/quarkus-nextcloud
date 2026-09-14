47b67420a23684a1526e75d9d29059ff8f25eeee
Stefan Richter-Huber <stefan@richter-huber.de>
2026-09-14 21:06:48 +0200

    Cleanup

--- runtime/src/main/java/io/github/stefanrichterhuber/nextcloudlib/runtime/clients/NextcloudWebhookRestClient.java
+++ runtime/src/main/java/io/github/stefanrichterhuber/nextcloudlib/runtime/clients/NextcloudWebhookRestClient.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.stefanrichterhuber.nextcloudlib.runtime.auth.NextcloudAPIAdminClientHeaders;
-import io.github.stefanrichterhuber.nextcloudlib.runtime.clients.NextcloudWebhookRestClient.WebhookMessage;
 import io.github.stefanrichterhuber.nextcloudlib.runtime.models.OCSMessage;
 import jakarta.annotation.Nonnull;
 import jakarta.ws.rs.Consumes;
